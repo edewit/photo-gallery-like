2946c747ea8bd3caf25fde1a7a3e8e1043e0ec9d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-12-11 13:47:44 +0100

    initial version of the front-end

---
+++ src/main/frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <like v-bind:photoId="1"></like>
+  </div>
+</template>
+
+<script>
+import like from "./components/like.vue";
+
+export default {
+  name: "App",
+  components: { like }
+};
+</script>
---
+++ src/main/frontend/src/components/like.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span class="fa-stack fa-sm" v-on:click="like()">
+      <i class="fa fa-circle fa-stack-2x blue-icon" />
+      <i v-bind:class="{ 'fa-thumbs-up': !done, 'fa-check-circle': done }" class="fa fa-stack-1x fa-inverse" />
+    </span>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      done: false,
+    };
+  },
+  props: {
+    photoId: Number
+  },
+  methods: {
+    like: function() {
+      axios.post("/likes", {
+        id: this.photoId,
+        likes: 1
+      }).then(() => this.done = true);
+    }
+  }
+};
+</script>
+
+<style scoped>
+span {
+  cursor: pointer;
+}
+.blue-icon {
+  color: #0099ff;
+}
+</style>
